6a3d8a87d208146404008612551a595aa34bc603
Sank <aserobabow95@mail.ru>
2017-08-23 10:41:31 +0600

    first

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var mainBowerFiles = require('main-bower-files');
+
+gulp.task('mainJS', function() {
+    return gulp.src(mainBowerFiles('**/*.js'))
+        .pipe(gulp.dest('dist/js'))
+});
+
+
+gulp.task('mainCSS', function() {
+    return gulp.src(mainBowerFiles('**/*.css', {
+      "overrides": {
+        "bootstrap": {
+            "main": [
+                "./dist/css/bootstrap.min.css",
+                "./dist/css/bootstrap-theme.min.css"
+                ]
+        }
+    }}))
+    .pipe(gulp.dest('dist/css'))
+});
